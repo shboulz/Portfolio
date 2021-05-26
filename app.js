abe1bf664dd8876ea12b1243c0aedc5b2c312e80
shboulz <shboul89@gmail.com>
2021-05-25 21:41:28 -0400

    adds email Commit

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 0,
+        "speed": 0.5,
         "direction": "none",
         "random": false,
         "straight": false,
